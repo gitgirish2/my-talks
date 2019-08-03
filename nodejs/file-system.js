cfd79cde984cc2eb3104c1a4b8dfa986d9d66c07
Ambati, Kirankumar <kambati@webmd.net>
2019-08-03 22:28:11 +0530

    chore: added code for NodeJS talk

---
+++ nodejs/file-system.js
@@ -0,0 +1,29 @@
+var fs = require('fs')
+
+// fs.writeFile('new-file.txt', 'Test Data', 'utf8', (err) => console.log('ERROR: ', err));
+
+// fs.readFile('new-file.txt', (err, data) => console.log('ERROR: ', err, '; DATA: ', data.toString()))
+
+// fs.appendFile('new-file.txt', '\nHello world!', 'utf8', (err) => console.log('ERROR: ', err));
+
+// fs.stat(__dirname, (err, stats) => {
+//     if (err) {
+//       console.error(err)
+//       return
+//     }
+//     console.log(stats.isFile());
+//     console.log(stats.isSymbolicLink());
+//     console.log(stats.isDirectory());
+//     console.log(stats.size);
+// });
+
+
+// fs.rename('new-file.txt', 'hello.txt', (err) => {
+//     if (err) throw err;
+//     console.log('Rename complete!');
+// });
+
+// fs.unlink('hello.txt', (err) => {
+//     if (err) throw err;
+//     console.log('new-file.txt is removed');
+// });
---
+++ nodejs/module-structure.js
@@ -0,0 +1,13 @@
+// console.log(process);
+
+// console.log(process.argv);
+
+// console.log(
+//     {
+//         __dirname,
+//         __filename,
+//         exports,
+//         module,
+//         require
+//     }
+// );
